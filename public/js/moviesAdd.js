8261448440e725990848c02a17a268e64e2bf286
Francisco Salanova <fsalanova.16@gmail.com>
2022-06-02 11:06:31 -0300

    Finalizacion de consigna sobre maquina de estado

--- public/js/moviesAdd.js
+++ public/js/moviesAdd.js
@@ -34,6 +34,37 @@ window.onload = function(){
                     console.log(estadoSecreto);
                 }
                 break;
+            case 2:            
+                if (tecla === 'c' || tecla === 'C') {
+                    estadoSecreto = 3;
+                    console.log(estadoSecreto);
+                }
+                break;
+            case 3:            
+                if (tecla === 'r' || tecla === 'R') {
+                    estadoSecreto = 4;
+                    console.log(estadoSecreto);
+                }
+                break;
+            case 4:            
+                if (tecla === 'e' || tecla === 'E') {
+                    estadoSecreto = 5;
+                    console.log(estadoSecreto);
+                }
+                break;
+            case 5:            
+                if (tecla === 't' || tecla === 'T') {
+                    estadoSecreto = 6;
+                    console.log(estadoSecreto);
+                }
+                break;
+            case 6:            
+                if (tecla === 'o' || tecla === 'O') {
+                    estadoSecreto = 0;
+                    console.log(estadoSecreto);
+                    alert('SECRETO MAGICO');
+                }
+                break;
             default:
                 estadoSecreto = 0;
                 console.log(estadoSecreto);

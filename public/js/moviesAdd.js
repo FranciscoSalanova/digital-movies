25b101328aebda6a2c96a6a9e12f1a9b0aaa42f8
Francisco Salanova <fsalanova.16@gmail.com>
2022-06-02 08:44:16 -0300

    Agregado de contenido js en archivo moviesAdd

---
+++ public/js/moviesAdd.js
@@ -0,0 +1,16 @@
+window.onload = function(){
+    let titulo = document.querySelector('.moviesAddTitulo')
+    let formulario = document.querySelector('#formulario');
+    let article = document.querySelector('article');
+    let campoTitulo = document.querySelector('#titulo');
+
+    titulo.innerHTML = 'AGREGAR PELÍCULA';
+    titulo.classList.add('titulo');
+    article.classList.add('fondoTransparente');
+    formulario.classList.add('fondoCRUD');
+
+    titulo.addEventListener('mouseover', () => {
+        titulo.style.color = '#' + Math.round(Math.random() * 999999).toString();
+    });
+
+}
